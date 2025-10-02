7f02cb6c05bdf3fde190c0bd96d7ce5eaad07f12
Alvaro Garcia <agarram0711@g.educaand.es>
2025-10-02 16:00:51 +0200

    Continuación ejercicios array

--- Unidad01/Arrays/Boletin1/Ej15.php
+++ Unidad01/Arrays/Boletin1/Ej15.php
@@ -1,1 +1,11 @@
-<?php
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ejercicio 15</title>
+</head>
+<body>
+
+</body>
+</html>
