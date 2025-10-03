1299fff275c6a7979c2fbd3bd70f975cd726ba3a
Alvaro Garcia <agarram0711@g.educaand.es>
2025-10-03 19:45:46 +0200

    Continuación ejercicios boletin

--- Unidad01/Arrays/Boletin1/Ej17.php
+++ Unidad01/Arrays/Boletin1/Ej17.php
@@ -1,1 +1,34 @@
-<?php
+<!doctype html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Ejercicio 17</title>
+    </head>
+    <body>
+        <?php
+            $vegetariano = ["Crema de tomate y verduras","Burrata sobre verduras asadas","Heura con girgolas"];
+            $precioVegetariano = [
+              "plato1" => "9€",
+              "plato2" => "12€",
+              "plato3" => "10€"
+            ];
+            $carnivoro = ["Entrecot a la placha", "Albóndigas con salsa de verdura", "Arroz con secreto iberico"];
+            $precioCarnivoro = [
+                "plato1" => "15€",
+                "plato2" => "11€",
+                "plato3" => "13€"
+            ];
+            $internacional = ["Tacos", "Sushi", "Ceviche"];
+            $precioInternacional = [
+                "plato1" => "6€",
+                "plato2" => "17€",
+                "plato3" => "14€"
+            ];
+            echo "<div id='vegetariano'>";
+            echo "<h2>Menú vegetariano</h2>";
+            echo "<";
+            echo "</div>";
+            ?>
+    </body>
+</html>
