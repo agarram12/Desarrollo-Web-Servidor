4b1984a652699b07c36f146ed3a0779510d78c58
Alvaro Garcia <agarram0711@g.educaand.es>
2025-11-04 16:05:02 +0100

    Enunciados

---
+++ Unidad02/Refuerzo/Enunciado1.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Enunciado</title>
+</head>
+<body>
+    <?php
+    $catalogo = [
+        "Ficción" => [
+            [
+                "titulo" => "Titulo 1",
+                "autor" => "Pepe",
+                "año" => 2010,
+                "ejemplares" => 200
+            ]
+        ],
+        "No ficción" => [
+            [
+                "titulo" => "Titulo 2",
+                "autor" => "Enrique Vila-Matas",
+                "año" => 2002,
+                "ejemplares" => 500
+            ]
+        ],
+        "Filosofia" => [
+            [
+                "titulo" => "Titulo 3",
+                "autor" => "Séneca",
+                "año" => 1830,
+                "ejemplares" => 2000
+            ]
+        ]
+    ];
+
+?>
+    <table>
+        <tr>
+            <th></th>
+        </tr>
+    </table>
+</body>
+</html>
