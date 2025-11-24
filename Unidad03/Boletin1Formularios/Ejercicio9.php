2db60fde03cf830e1785e8d4ad50ce9effba80ec
Alvaro Garcia <agarram0711@g.educaand.es>
2025-11-24 17:45:27 +0100

    Ejercicio9.php por hacer

---
+++ Unidad03/Boletin1Formularios/Ejercicio9.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Ejercicio 9</title>
+        <style>
+            table {
+                border-collapse: collapse;
+                border-bottom: 1px solid white;
+                color: white;
+                background-color: black;
+            }
+        </style>
+    </head>
+    <body>
+        <form name="numeros" method="get" action="">
+            <input name="num1" type="number">
+            <input name="num2" type="number">
+            <input name="num3" type="number">
+            <input name="num4" type="number">
+        </form>
+        <table>
+            <tr>
+                <th>Número</th>
+                <th>Cuadrado</th>
+                <th>Cubo</th>
+            </tr>
+            <?php
+
+            ?>
+        </table>
+    </body>
+</html>
