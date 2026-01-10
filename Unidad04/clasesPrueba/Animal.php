00a92f7c4fd336bff1cf692133e7dbcb29805bde
AlvaroGarcia <agarram0711@g.educaand.es>
2026-01-10 14:16:13 +0100

    Primer ejercicio clases php

--- Unidad04/clasesPrueba/Animal.php
+++ Unidad04/clasesPrueba/Animal.php
@@ -1,9 +1,17 @@
 <?php
     class Animal {
+        // añadimos los 
         public $nombre;
         public $color;
         public $fechaNac;
-    
+        // constructor para aceptar los datos como parametros
+        public function __construct($nombre, $color, $fechaNac) {
+            $this->nombre = $nombre;
+            $this->color = $color;
+            $this->fechaNac = $fechaNac;
+
+        }
+
         public function __get($propiedad)
         {
             return $this->$propiedad;
@@ -13,24 +21,25 @@
         {
             $this->$propiedad = $valor;
         }
-
+        // obtener el año para calcular la edad del animal
         public function verEdad()
         {
-            $fechaNacimiento = DateTime::createFromFormat('Y-M-D', $this->fechaNac);
+            $fechaNacimiento = DateTime::createFromFormat('Y-m-d', $this->fechaNac);
             $hoy = new DateTime();
             $edad = $hoy->diff($fechaNacimiento);
             return (int) $edad->y;
         }
-        
+        // método __tostring() para mostrar los datos del animal
         public function __toString()
         {
             $edad = $this->verEdad();
             return "Animal: " . $this->nombre . "\n" .
             "Color: " . $this->color . "\n" .
             "Fecha Nacimiento: " . $this->fechaNac . "\n" .
-            "Edad: " . $edad . " año"($edad == 1 ? "" : "s") . "\n";
+            "Edad: " . $edad . " año" . ($edad == 1 ? "" : "s") . "\n";
         }
     }
-    
-    
+    // crear nuevo animal
+    $a1 = new Animal("Lala", "Marrón", "2018-08-15");
+    echo $a1;
 ?>
