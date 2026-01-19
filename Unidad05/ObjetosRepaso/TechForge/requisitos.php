7803573193cdfc25bf5afb92d95439d40b71d47c
Kllmplz <alvarogarciaramos98@gmail.com>
2026-01-19 15:16:41 +0100

    Cambios y nuevo ejercicio PHPOO

---
+++ Unidad05/ObjetosRepaso/TechForge/requisitos.php
@@ -0,0 +1,45 @@
+<?php 
+    interface evaluable {
+        public function esRentable();
+        public function impactoAmbiental();
+        public function descripcionTecnica();
+    }
+    
+    abstract class Invento {
+        protected String $nombre;
+        protected String $proposito;
+        protected String $fechaPrototipo;
+        protected float $coste;
+        protected $materiales = [];
+        protected static int $totalinventos = 0;
+
+        public function __construct($nombre, $proposito, $fechaPrototipo, $coste, $materiales) {
+            if (!preg_match('/^\d{4}-\d{2}-\d{2}$/', $fechaPrototipo)) {
+                throw new Exception("Fecha del prototipo no válida", $fechaPrototipo);
+            }
+            if ($coste < 0) {
+                throw new Exception("Coste del prototipo no válido", $coste);
+            }
+            $this->nombre = $nombre;
+            $this->proposito = $proposito;
+            $this->fechaPrototipo = $fechaPrototipo;
+            $this->coste = $coste;
+            $this->materiales = $materiales;
+            self::$totalinventos++;
+        }
+
+        public function anyoDesdePrototipo() {
+            $fechaPrototipo = new DateTime(($this->fechaPrototipo));
+            $fechaActual = new DateTime();
+            $anyo = $fechaActual->diff($fechaPrototipo)->y;
+        }
+
+        public function __toString() {
+            $fechaFormateada = date("d/m/Y", strtotime($this->fechaPrototipo));
+            return "invento: {$this->nombre} | prototipo: {$fechaFormateada} | Coste: {$this->coste}";
+
+        }
+
+        abstract public function calcularComplejidad();
+    }
+?>
--- Unidad05/Práctica1PHPOO/clases.php
+++ Unidad05/Práctica1PHPOO/clases.php
@@ -1,5 +1,5 @@
 <?php
-    interface certificable{
+    interface certificable {
         public function obtenerEtiqueta();
     }
     
