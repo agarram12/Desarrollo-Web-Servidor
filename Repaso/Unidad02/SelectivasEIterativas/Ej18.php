7e3e24efbd62b1ce4e5ab02ca51f2f3891f3aeea
Álvaro García <agarram0711@g.educaand.es>
2026-05-07 13:29:35 +0200

    Cambiar el ejercicio

---
+++ Repaso/Unidad02/SelectivasEIterativas/Ej18.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <?php
+            $arr = [3,8,7,-6];
+            foreach ($arr as $elemento) {
+
+            }
+        ?>
+    </body>
+</html>
